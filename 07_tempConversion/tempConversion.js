1723a81fce3b3efa98abcdb3678af83a04c58bae
JoelCruzCode <136981207+JoelCruzCode@users.noreply.github.com>
2023-07-07 05:00:42 -0700

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  const celsius = Number((((temp - 32) * 5) / 9).toFixed(1));
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  const f = Number(((temp * 9) / 5 + 32).toFixed(1));
+  return f;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
